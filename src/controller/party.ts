888716aba11a8b8feaae99a2e3015a4a1190afd8
Jack Frate <pub@DESKTOP-I06QLN1.localdomain>
2018-11-06 01:11:19 -0500

    configured some more things

---
+++ src/controller/party.ts
@@ -0,0 +1,11 @@
+import { BaseContext } from 'koa';
+
+
+export default class PartyController {
+    public static async getParty(ctx: BaseContext) {
+
+
+        // stubbed out method
+        ctx.status = 200;
+    }
+}
